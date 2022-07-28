c48a3d83ab81b33e84b5c643062bb820311b0a24
nickolay7 <shkelal50@gmail.com>
2022-07-28 15:29:10 +0300

    Updates

--- src/api/contextProvider/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./useAppContext";
-export * from "./appContext";
--- src/api/contextProvider/useAppContext.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react';
-import { Context } from "./appContext";
-
-export const useAppContext = () => useContext(Context);
